acafee2225c5b4bfbab41a8e454e27b440604b2d
talves <tony@3alves.com>
2022-08-09 10:38:30 -0700

    update: use createSignal, createEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import logo from "./logo.svg";
 import styles from "./App.module.css";
-import { createSignal } from "solid-js";
+import { createSignal, createEffect } from "solid-js";
 
 function App() {
   const [count, setCount] = createSignal(1);
+  const [multiplier, setMultiplier] = createSignal(1);
+  const [value, setValue] = createSignal(0);
 
   setInterval(() => {
     if (count() >= 60) {
       setCount(0);
     } else setCount(count() + 1);
   }, 1000);
+
+  setInterval(() => {
+    setMultiplier(multiplier() + 1);
+  }, 2500);
+
+  createEffect(() => {
+    setValue(multiplier() * count());
+  });
 
   return (
     <div class={styles.App}>
@@ -26,7 +36,7 @@ function App() {
         >
           Learn Solid
         </a>{" "}
-        [{count}]
+        [{value()}]
       </header>
     </div>
   );
